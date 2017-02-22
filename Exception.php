fbebe4f54d8e6d2a43988c9da9b91105973e955e
Heropoo <aiyouyou1000@163.com>
2017-02-22 16:57:51 +0800

    submit

---
+++ Exception.php
@@ -0,0 +1,15 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: ttt
+ * Date: 2017/2/14
+ * Time: 10:32
+ */
+
+namespace coco\config;
+
+
+class Exception extends \Exception
+{
+
+}
---
+++ helpers.php
@@ -0,0 +1,16 @@
+<?php
+
+if(!function_exists('config')){
+    /**
+     * get a config
+     * @param string $key
+     * @param bool $throw
+     * @return mixed|null
+     */
+    function config($key, $throw = false){
+        return coco\config\Config::get($key, $throw);
+    }
+}
+
+
+
